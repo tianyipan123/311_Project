9a106905dbf54ea232b0c0f298fd99944b3dc625
Tianyi Pan <nappan363@gmail.com>
2023-03-23 22:36:31 -0400

    fix several bugs
    - data load exception
    - speed up majority prediction
    - restructure some code

--- part_a/ensemble.py
+++ part_a/ensemble.py
@@ -1,5 +1,3 @@
-import sys, os
-sys.path.append(os.getcwd())
 from utils import *
 from sklearn.utils import resample
 from sklearn.impute import KNNImputer
@@ -7,10 +5,12 @@ from math import ceil
 
 import item_response
 
+
 def bootstrap(data):
     """Returns a dataset sampled with replacement from the given data.
     """
     return resample(data)
+
 
 def train_knn(data, k):
     """Randomly samples the data, and returns a trained KNN model with hyperparameter k.
@@ -19,8 +19,9 @@ def train_knn(data, k):
     mat = nbrs.fit_transform(bootstrap(data).toarray())
     return mat
 
+
 def train_irt(data, lr, iterations):
-    """Randomly samples the data, and returns a trained IRT model with hyperparemeters 
+    """Randomly samples the data, and returns a trained IRT model with hyperparemeters
     lr and iterations.
     """
     data = bootstrap(data)
@@ -32,17 +33,19 @@ def train_irt(data, lr, iterations):
             theta, beta = item_response.update_theta_beta(data, lr, theta, beta)
     return theta, beta
 
+
 def knn_predict(train_data, valid_data, k):
-     """Trains a KNN model with given k on train_data, then 
-     returns the predictions on valid_data.
-     """
-     matrix = train_knn(train_data, k)
-     return sparse_matrix_predictions(valid_data, matrix)
-     
+    """Trains a KNN model with given k on train_data, then
+    returns the predictions on valid_data.
+    """
+    matrix = train_knn(train_data, k)
+    return sparse_matrix_predictions(valid_data, matrix)
+
+
 def IRT_predict(train_data, valid_data, lr, iterations):
-    """Trains an IRT model with hyperparameters lr, iterations on train_data, then 
+    """Trains an IRT model with hyperparameters lr, iterations on train_data, then
     returns predictions on valid_data.
-    
+
     :param valid_data: A dictionary {user_id: list, question_id: list,
     is_correct: list}
     """
@@ -56,23 +59,26 @@ def IRT_predict(train_data, valid_data, lr, iterations):
 
     return np.array(pred)
 
+
 def majority(*predictions):
-    majority = []
+    majority = np.array(predictions, dtype=int)
+    assert majority.shape[0] == len(predictions)
     half = ceil(len(predictions) / 2)
 
-    for i in range(0, len(predictions[0])):
-        count = 0
-        for pred in predictions:
-            if pred[i]:
-                    count += 1
+    # for i in range(0, len(predictions[0])):
+    #     count = 0
+    #     for pred in predictions:
+    #         if pred[i]:
+    #                 count += 1
+    #
+    #     majority.append(count >= half)
+    return majority.sum(axis=0) >= half
 
-        majority.append(count >= half)
-    return np.array(majority)
 
 def main():
-    sparse_matrix = load_train_sparse("data")
-    val_data = load_valid_csv("data")
-    test_data = load_public_test_csv("data")
+    sparse_matrix = load_train_sparse("../data")
+    val_data = load_valid_csv("../data")
+    test_data = load_public_test_csv("../data")
 
     # Hyperparameters for IRT
     lr = 0.002
@@ -94,4 +100,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
+
